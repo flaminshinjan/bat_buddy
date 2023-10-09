294f410e6d3a109125b70eb72a0b795e510a3426
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-09 21:06:39 +0530

    Battery fetching functionality achieved

--- farmako_task/ContentView.swift
+++ farmako_task/ContentView.swift
@@ -1,51 +1,49 @@
-//
-//  ContentView.swift
-//  farmako_task
-//
-//  Created by Shinjan Patra on 09/10/23.
-//
-
 import SwiftUI
 
-
 struct ContentView: View {
-    @State private var batteryPercentage: Int? = nil
+    @State private var batteryPercentage: Int?
     
     var body: some View {
         ZStack {
-            Color(.black)
+            Color(Color.black)
+                .scaledToFill()
                 .ignoresSafeArea()
-            Image("horse 1")
-                .resizable()
-                .scaledToFit()
             VStack {
-                
-                Spacer()
-                
-                
+                Image("horse 1")
+                    .resizable()
+                    .scaledToFit()
                 
                 Button(action: {
-                    
+                    fetchBatteryPercentage()
                 }) {
-                    Text("Fetch Battery")
-                        .padding(EdgeInsets(top: 10, leading: 50, bottom: 10, trailing: 50))
-                        .background(Color.orange.opacity(0.9))
+                    Text("Fetch Battery Percentage")
+                        .padding()
+                        .background(Color.green)
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 }
                 .padding()
-                
-                
-                Spacer()
-                Text("Battery Percentage: \(batteryPercentage ?? 0)%")
+                Text("Battery Percentage \(batteryPercentage ?? 0)%")
                     .bold()
                     .padding()
                     .foregroundColor(.white)
+                
+            }
+            .onAppear {
+                fetchBatteryPercentage()
             }
         }
     }
     
-    
+    func fetchBatteryPercentage() {
+        // Call Objective-C function to fetch battery percentage
+        let percentage = BatteryInfoFetcher.fetchBatteryPercentage()
+        
+        // Update the battery percentage in the UI
+        DispatchQueue.main.async {
+            self.batteryPercentage = percentage
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
