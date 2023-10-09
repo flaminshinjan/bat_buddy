93798815f3ce82798919b7e52a5cacc81699900f
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-09 15:43:13 +0530

    Initial Commit

---
+++ farmako_task/farmako_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  farmako_taskApp.swift
+//  farmako_task
+//
+//  Created by Shinjan Patra on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct farmako_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ farmako_taskUITests/farmako_taskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  farmako_taskUITestsLaunchTests.swift
+//  farmako_taskUITests
+//
+//  Created by Shinjan Patra on 09/10/23.
+//
+
+import XCTest
+
+final class farmako_taskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
